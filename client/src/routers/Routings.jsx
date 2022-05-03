629a50188287bda27e1a200cc4afe4d7a217e26a
albaajouri <mohamed.bajouri@hotmail.com>
2022-05-03 13:33:06 +0200

    first push

--- client/src/routers/Routings.jsx
+++ client/src/routers/Routings.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/pages/Home";
 import Questions from "../components/pages/Questions";
+import Settings from "../components/singleplayer/settings/Settings";
 
 const Routings = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/questions" element={<Questions />} />
+      <Route path="/settings" element={<Settings />} />
     </Routes>
   </Router>
 );
