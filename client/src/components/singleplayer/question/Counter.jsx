2ffc8781caf721f15eb3059a6dcccc3a865b5ba8
Luca Emanuel <luca@kuhs.org>
2022-05-09 17:54:06 +0200

    styling3000...

--- client/src/components/singleplayer/question/Counter.jsx
+++ client/src/components/singleplayer/question/Counter.jsx
@@ -1,32 +1,31 @@
-import React from "react";
-import { useEffect, useContext } from "react";
-import MyContext from "../../../context/MyContext";
-import "../../../styling/rewards.css";
+import { useEffect, useContext } from 'react';
+import MyContext from '../../../context/MyContext';
+import '../../../styling/rewards.css';
 
 const Counter = () => {
-  const context = useContext(MyContext);
-  const {
-    color,
+	const context = useContext(MyContext);
+	const {
+		color,
 
-    score,
-    setScore,
-    newQuestion,
-    hints,
-    setHints,
-  } = context;
-  // console.log("hints", hints);
-  // useEffect(() => {
-  //   color === "green" && newQuestion === false && setScore((prev) => prev + 10);
-  // }, [color]);
-  useEffect(() => {
-    score !== 0 && score % 50 === 0 && setHints((prev) => (prev + 1));
-  }, [score]);
-  return (
-    <div className="Counter">
-      <label>SCORE : </label>
-      <span>{score}</span>
-    </div>
-  );
+		score,
+		setScore,
+		newQuestion,
+		hints,
+		setHints,
+	} = context;
+	// console.log("hints", hints);
+	// useEffect(() => {
+	//   color === "green" && newQuestion === false && setScore((prev) => prev + 10);
+	// }, [color]);
+	useEffect(() => {
+		score !== 0 && score % 50 === 0 && setHints((prev) => prev + 1);
+	}, [score]);
+	return (
+		<div className='Counter'>
+			<label>SCORE : </label>
+			<span>{score}</span>
+		</div>
+	);
 };
 
 export default Counter;
--- client/src/context/MyProvider.jsx
+++ client/src/context/MyProvider.jsx
@@ -1,107 +1,99 @@
-import React, { useEffect } from "react";
-import MyContext from "./MyContext";
-import { useState } from "react";
-import "../../src/App.css";
-import useFetch from "../hooks/useFetch";
+import { useEffect } from 'react';
+import MyContext from './MyContext';
+import { useState } from 'react';
+import '../../src/App.css';
+import useFetch from '../hooks/useFetch';
 
 const MyProvider = ({ children }) => {
-  const [message, setMessage] = useState();
-  const [color, setColor] = useState();
-  const [rightAnswer, setRightAnswer] = useState();
-  const [score, setScore] = useState(0);
-  const [category, setCategory] = useState();
-  const [difficulty, setDifficulty] = useState();
-  const [gameMode, setGameMode] = useState();
+	const [message, setMessage] = useState();
+	const [color, setColor] = useState();
+	const [rightAnswer, setRightAnswer] = useState();
+	const [score, setScore] = useState(0);
+	const [category, setCategory] = useState();
+	const [difficulty, setDifficulty] = useState();
+	const [gameMode, setGameMode] = useState();
 
-  const [number, setNumber] = useState(10);
-  const [allAnswers, setAllAnswers] = useState();
-  const [correctAnswer, setCorrectAnswer] = useState();
-  const [incorrect, setIncorrect] = useState();
-  const [quest, setQuest] = useState([]);
+	const [number, setNumber] = useState(10);
+	const [allAnswers, setAllAnswers] = useState();
+	const [correctAnswer, setCorrectAnswer] = useState();
+	const [incorrect, setIncorrect] = useState();
+	const [quest, setQuest] = useState([]);
 
+	const [diff, setDiff] = useState('easy');
+	const [cat, setCat] = useState('arts');
+	const [randomAnswers, setRandomAnswers] = useState([]);
 
+	const [hints, setHints] = useState(0);
 
-  const [diff, setDiff] = useState("easy");
-  const [cat, setCat] = useState("arts");
-  const [randomAnswers, setRandomAnswers] = useState([]);
+	const [email, setEmail] = useState('');
+	const [pass, setPass] = useState('');
 
-  const [hints, setHints] = useState(0);
+	const [questionArray, setQuestionArray] = useState();
 
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+	const [wrongAnswers, setWrongAnswers] = useState();
+	const [indexCounter, setIndexCounter] = useState(0);
+	const [gameOver, setGameOver] = useState(true);
 
-  const [questionArray, setQuestionArray] = useState();
+	const url = `https://the-trivia-api.com/api/questions?limit=${number}&&categories=${cat}&&difficulty=${diff}`;
+	const initialState = { results: null, loading: true, eror: null };
+	const { results, loading, eror } = useFetch(url, initialState);
+	// const questionArray = results.map((item) => item.question); //set state to everything
+	//results.map((item) => setQuestionArray(item.question));
 
-  
+	console.log(results);
 
-  const [wrongAnswers, setWrongAnswers] = useState();
-  const [indexCounter, setIndexCounter] = useState(0);
-  const [gameOver, setGameOver] = useState(true);
+	if (loading) return <p>loading ..</p>;
+	if (eror) return <p>{eror}</p>;
 
+	return (
+		<MyContext.Provider
+			value={{
+				questionArray,
 
+				indexCounter,
+				setIndexCounter,
+				wrongAnswers,
+				setWrongAnswers,
 
+				randomAnswers,
+				setRandomAnswers,
+				results,
+				loading,
+				eror,
+				cat,
+				gameOver,
+				setGameOver,
+				setCat,
+				diff,
+				setDiff,
+				message,
+				setMessage,
+				color,
+				setColor,
+				rightAnswer,
+				setRightAnswer,
+				score,
+				setScore,
 
-  const url = `https://the-trivia-api.com/api/questions?limit=${number}&&categories=${cat}&&difficulty=${diff}`;
-  const initialState = { results: null, loading: true, eror: null };
-  const { results, loading, eror } = useFetch(url, initialState);
-  // const questionArray = results.map((item) => item.question); //set state to everything
-  //results.map((item) => setQuestionArray(item.question));
-
-  console.log(results);
-
-  if (loading) return <p>loading ..</p>;
-  if (eror) return <p>{eror}</p>;
-
-  return (
-    <MyContext.Provider
-      value={{
-
-        questionArray,
-
-        indexCounter, 
-        setIndexCounter,
-        wrongAnswers, 
-        setWrongAnswers,
-
-        randomAnswers,
-        setRandomAnswers,
-        results,
-        loading,
-        eror,
-        cat,
-        gameOver,
-        setGameOver,
-        setCat,
-        diff,
-        setDiff,
-        message,
-        setMessage,
-        color,
-        setColor,
-        rightAnswer,
-        setRightAnswer,
-        score,
-        setScore,
-       
-        category,
-        setCategory,
-        difficulty,
-        setDifficulty,
-        gameMode,
-        setGameMode,
-        number,
-        setNumber,
-        hints,
-        setHints,
-        email,
-        setEmail,
-        pass,
-        setPass,
-      }}
-    >
-      {children}
-    </MyContext.Provider>
-  );
+				category,
+				setCategory,
+				difficulty,
+				setDifficulty,
+				gameMode,
+				setGameMode,
+				number,
+				setNumber,
+				hints,
+				setHints,
+				email,
+				setEmail,
+				pass,
+				setPass,
+			}}
+		>
+			{children}
+		</MyContext.Provider>
+	);
 };
 
 export default MyProvider;
