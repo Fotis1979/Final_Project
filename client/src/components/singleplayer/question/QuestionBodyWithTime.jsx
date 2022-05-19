09541fa948a63f17d11d0f65c98821ea21dcc23a
walaa.alkasim <walaa.alkasim@hotmail.com>
2022-05-19 15:05:13 +0200

    commits

--- client/src/components/singleplayer/question/QuestionBodyWithTime.jsx
+++ client/src/components/singleplayer/question/QuestionBodyWithTime.jsx
@@ -13,153 +13,156 @@ import Rewards from "../rewards/Rewards";
 import Nav from "../../pages/Nav";
 import Nav2 from "../../pages/Nav2";
 
-
-import '../../../styling/questions.css';
-import Popup from '../../Popup/Popup';
-import ErrorMessage from '../../errorMessage/ErrorMessage';
+import "../../../styling/questions.css";
+import Popup from "../../Popup/Popup";
+import ErrorMessage from "../../errorMessage/ErrorMessage";
 //import '../../../styling/questionBodyWithTime.css';
 
 const QuestionBody = () => {
-	const context = useContext(MyContext);
-	const {
-		loading,
-		number,
-		setNumber,
-		eror,
-		hints,
-		setHints,
-		results,
-		email,
-		seconds,
-		setSeconds,
-		wrongAnswers,
-		rightAnswer,
-		questionArray,
-		randomAnswers,
-		setRandomAnswers,
-		cat,
-		setCat,
-		score,
-		setScore,
-		setWrongAnswers,
-		answerPopup,
-		setAnswerPopup,
-	} = context;
+  const context = useContext(MyContext);
+  const {
+    loading,
+    number,
+    setNumber,
+    eror,
+    hints,
+    setHints,
+    results,
+    email,
+    seconds,
+    setSeconds,
+    wrongAnswers,
+    rightAnswer,
+    questionArray,
+    randomAnswers,
+    setRandomAnswers,
+    cat,
+    setCat,
+    score,
+    setScore,
+    setWrongAnswers,
+    answerPopup,
+    setAnswerPopup,
+  } = context;
 
-	const [error, setError] = useState(false);
-	const [selected, setSelected] = useState();
-	const [indexCounter, setIndexCounter] = useState(0);
+  const [error, setError] = useState(false);
+  const [selected, setSelected] = useState();
+  const [indexCounter, setIndexCounter] = useState(0);
 
-	// console.log(questionArray);
-	// console.log(wrongAnswers);
-	// console.log(rightAnswer);
+  // console.log(questionArray);
+  // console.log(wrongAnswers);
+  // console.log(rightAnswer);
 
-	const nav = useNavigate();
+  const nav = useNavigate();
 
-	indexCounter === number - 1 + 1 && nav('/game_over');
+  indexCounter === number - 1 + 1 && nav("/game_over");
 
-	const handleSelect = (i) => {
-		if (selected === i && selected === rightAnswer[indexCounter])
-			return 'select';
-		else if (selected === i && selected !== rightAnswer[indexCounter])
-			return 'wrong';
-		else if (i === rightAnswer[indexCounter]) return 'select';
-	};
-	const handleCheck = (i) => {
-		setSelected(i);
-		setAnswerPopup(true);
-		if (i === rightAnswer[indexCounter]) setScore(score + 10);
-		setError(false);
-	};
+  const handleSelect = (i) => {
+    if (selected === i && selected === rightAnswer[indexCounter])
+      return "select";
+    else if (selected === i && selected !== rightAnswer[indexCounter])
+      return "wrong";
+    else if (i === rightAnswer[indexCounter]) return "select";
+  };
+  const handleCheck = (i) => {
+    setSelected(i);
+    setAnswerPopup(true);
+    if (i === rightAnswer[indexCounter]) setScore(score + 10);
+    setError(false);
+  };
 
-	const nextHandler = () => {
-		// console.log("first");
-		if (selected) {
-			setIndexCounter((prevIndexCounter) => prevIndexCounter + 1);
-			setSelected();
-		} else setError(true);
-		
-	};
+  const nextHandler = () => {
+    // console.log("first");
+    if (selected) {
+      setIndexCounter((prevIndexCounter) => prevIndexCounter + 1);
+      setSelected();
+    } else setError(true);
+  };
 
-	const answers = [];
-	answers.push(rightAnswer[indexCounter]);
+  const answers = [];
+  answers.push(rightAnswer[indexCounter]);
 
-	wrongAnswers[indexCounter].map((el) => answers.push(el));
+  wrongAnswers[indexCounter].map((el) => answers.push(el));
 
-	console.log(answers);
+  console.log(answers);
 
-	useEffect(() => {
-		setRandomAnswers(arrayRandomize(answers));
-	}, [indexCounter]);
+  useEffect(() => {
+    setRandomAnswers(arrayRandomize(answers));
+  }, [indexCounter]);
 
-	useEffect(() => {
-		seconds === 15 &&
-			setIndexCounter((prevIndexCounter) => prevIndexCounter + 1);
-	}, [seconds]);
+  useEffect(() => {
+    seconds === 15 &&
+      setIndexCounter((prevIndexCounter) => prevIndexCounter + 1);
+  }, [seconds]);
 
-	// !**********!***************!******!********!
-	// const x = (e) => {
-	//   setCat(e.target.value);
-	// };
-	// !**********!***************!******!********!
+  // !**********!***************!******!********!
+  // const x = (e) => {
+  //   setCat(e.target.value);
+  // };
+  // !**********!***************!******!********!
 
-	// console.log(answers);
-	// console.log("answers are :", answers);
-	// console.log(randomAnswers);
-	console.log(results[indexCounter].correctAnswer);
-	// console.log(results[indexCounter].category);
+  // console.log(answers);
+  // console.log("answers are :", answers);
+  // console.log(randomAnswers);
+  console.log(results[indexCounter].correctAnswer);
+  // console.log(results[indexCounter].category);
 
-	if (loading) return <p>loading ..</p>;
-	if (eror) return <p>{eror}</p>;
+  if (loading) return <p>loading ..</p>;
+  if (eror) return <p>{eror}</p>;
 
-	return (
-		<>
-			<Nav />
-			<Rewards />
-			
-			{error && <ErrorMessage>Please select an option first</ErrorMessage>}
-			<div className='qa--section'>
-				<div className='questions--section'>
-					Q{indexCounter + 1} . {questionArray[indexCounter]}
-				</div>
-				<div className='answers--section'>
-					{randomAnswers.map((el, index) => (
-						<div key={index} className='align-items'>
-							<button
-								value={el}
-								className={`singleOption  ${selected && handleSelect(el)}`}
-								key={el}
-								onClick={() => handleCheck(el)}
-								disabled={selected}
-							>
-								{index + 1 + '.' + el}
-							</button>
-						</div>
-					))}
-				</div>
-				{(hints === 1 || hints === 2) && (
-					<button
-						className='rewards--btn'
-						onClick={() => randomAnswers.pop() && setHints((prev) => prev - 1)}
-					>
-						{console.log(randomAnswers)}{' '}
-						{hints === 2 ? 'DoubleClick for 50/50 CHANCE' : 'useHint'}
-					</button>
-				)}
-				<button className='next--btn' onClick={nextHandler}>
-					next
-				</button>
-				{selected === rightAnswer[indexCounter]&&<Popup trigger={answerPopup} setTrigger={setAnswerPopup}>
-					<p>Correct answer</p></Popup>}
-				{selected !== rightAnswer[indexCounter]&&<Popup trigger={answerPopup} setTrigger={setAnswerPopup}>
-					<p>wrong answer</p></Popup>}
-				<QuestionTimer />
-				<QuestionCounter />
-				{/*<Counter />*/}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Nav />
+      <Rewards />
 
+      {error && <ErrorMessage>Please select an option first</ErrorMessage>}
+      <div className="qa--section">
+        <div className="questions--section">
+          Q{indexCounter + 1} . {questionArray[indexCounter]}
+        </div>
+        <div className="answers--section">
+          {randomAnswers.map((el, index) => (
+            <div key={index} className="align-items">
+              <button
+                value={el}
+                className={`singleOption  ${selected && handleSelect(el)}`}
+                key={el}
+                onClick={() => handleCheck(el)}
+                disabled={selected}
+              >
+                {index + 1 + "." + el}
+              </button>
+            </div>
+          ))}
+        </div>
+        {(hints === 1 || hints === 2) && (
+          <button
+            className="rewards--btn"
+            onClick={() => randomAnswers.pop() && setHints((prev) => prev - 1)}
+          >
+            {console.log(randomAnswers)}{" "}
+            {hints === 2 ? "DoubleClick for 50/50 CHANCE" : "useHint"}
+          </button>
+        )}
+        <button className="next--btn" onClick={nextHandler}>
+          next
+        </button>
+        {selected === rightAnswer[indexCounter] && (
+          <Popup trigger={answerPopup} setTrigger={setAnswerPopup}>
+            <p>Correct answer</p>
+          </Popup>
+        )}
+        {selected !== rightAnswer[indexCounter] && (
+          <Popup trigger={answerPopup} setTrigger={setAnswerPopup}>
+            <p>wrong answer</p>
+          </Popup>
+        )}
+        <QuestionTimer />
+        <QuestionCounter />
+        {/*<Counter />*/}
+      </div>
+    </>
+  );
 };
 
 export default QuestionBody;
--- client/src/context/MyProvider.jsx
+++ client/src/context/MyProvider.jsx
@@ -12,41 +12,31 @@ const MyProvider = ({ children }) => {
   const [difficulty, setDifficulty] = useState();
   const [gameMode, setGameMode] = useState();
   const [seconds, setSeconds] = useState(0);
-
   const [error, setError] = useState(false);
   const [number, setNumber] = useState(10);
   const [allAnswers, setAllAnswers] = useState();
   const [correctAnswer, setCorrectAnswer] = useState();
   const [incorrect, setIncorrect] = useState();
   const [quest, setQuest] = useState([]);
-
   const [diff, setDiff] = useState("easy");
   const [cat, setCat] = useState("arts");
   const [randomAnswers, setRandomAnswers] = useState([]);
   const [hints, setHints] = useState(0);
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
-
   const [questionArray, setQuestionArray] = useState([]);
   const [wrongAnswers, setWrongAnswers] = useState([]);
   const [rightAnswer, setRightAnswer] = useState([]);
-
   const [answers, setAnswers] = useState([]);
-
-
-	const [indexCounter, setIndexCounter] = useState(0);
-	const [gameOver, setGameOver] = useState(true);
-	const [answerPopup, setAnswerPopup] = useState(false);
-
-
+  const [indexCounter, setIndexCounter] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  const [answerPopup, setAnswerPopup] = useState(false);
   const [name, setName] = useState("");
   const [isProfileSaved, setIsProfileSaved] = useState(false);
   const [birthDate, setBirthDate] = useState("");
   const [loginMsg, setLoginMsg] = useState("");
-
   const [highScoreResult, setHighScoreResult] = useState(0);
   const [highScore, setHighScore] = useState(0);
-
   const url = `https://the-trivia-api.com/api/questions?limit=${number}&&categories=${cat}&&difficulty=${diff}`;
   const initialState = { results: null, loading: true, eror: null };
   const { results, loading, eror } = useFetch(url, initialState);
@@ -62,7 +52,6 @@ const MyProvider = ({ children }) => {
 
   if (loading) return <p>loading ..</p>;
   if (eror) return <p>'eror'</p>;
-
 
   return (
     <MyContext.Provider
@@ -126,14 +115,13 @@ const MyProvider = ({ children }) => {
         setLoginMsg,
         name,
         setName,
-    	answerPopup,
-				setAnswerPopup
+        answerPopup,
+        setAnswerPopup,
       }}
     >
       {children}
     </MyContext.Provider>
   );
-
 };
 
 export default MyProvider;
