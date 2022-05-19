baaeda1dc142809f388c62d969db60c67302320a
Fotis-DCI <fotgis@yahoo.com>
2022-05-19 15:01:39 +0200

    fotis new commit

--- client/src/components/singleplayer/question/QuestionTimer.jsx
+++ client/src/components/singleplayer/question/QuestionTimer.jsx
@@ -30,7 +30,6 @@ const QuestionTimer = () => {
 		// message && setSeconds(0);
 	}, [seconds]);
 
-<<<<<<< HEAD
   useEffect(() => {
     gameDiff === "easy" && seconds === 21 && setSeconds(0);
     gameDiff === "medium" && seconds === 16 && setSeconds(0);
@@ -52,26 +51,6 @@ const QuestionTimer = () => {
       </span>
     </aside>
   );
-=======
-	// useEffect(() => {
-	//   newQuestion &&
-	//     fetch(
-	//       `https://the-trivia-api.com/api/questions?limit=50&difficulty=${diff}&&categories=${cat}`
-	//     )
-	//       .then((res) => res.json())
-	//       .then((data) => setQuestions(data[Math.floor(Math.random() * 50)]));
-
-	//   setRightAnswer("");
-	// }, [newQuestion]);
-
-	return (
-		<div className='timer'>
-			<span className='sec--sec'>{seconds}</span>
-
-			<span>U have 15 secs for each Question !</span>
-		</div>
-	);
->>>>>>> c5ce1659e4d70eafd9972bd0b74a339bcde0dc1e
 };
 
 export default QuestionTimer;
